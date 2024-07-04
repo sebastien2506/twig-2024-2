133f2099ed8f8b9dc66e03d329660d4184367998
WebDevCF2m <gitweb@cf2m.be>
2024-07-04 14:39:32 +0200

    index

--- public/index.php
+++ public/index.php
@@ -1,1 +1,25 @@
 <?php
+// chemins vers les classes Twig
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// chargement de l'autoload de composer
+require_once "../vendor/autoload.php";
+
+$loader = new FilesystemLoader('../view');
+$twig = new Environment($loader,[
+    // on désactive le cache en développement
+    'cache' => false,
+    // on affiche le debogage en développement
+    'debug' => true,
+]);
+
+$tab = ["un","deux","trois","quatre"];
+
+// chargement d'un template se trouvant dans view
+echo $twig->render('test.html.twig',[
+    "message" => "Hello World!",
+    "tab" => $tab,
+]);
+
+
